4999d7edc3218639437ca66fdca6f0400bf86868
vedanth <vedanths411@gmail.com>
2026-01-12 00:26:50 -0700

    Added Monte-Carlo Simulation, added drag and SRP torques

--- main.py
+++ main.py
@@ -35,11 +35,14 @@ Approximate the system as a linear system about equillibrium and use te Jacobian
 
 """
 import numpy as np
+import time
 import ppigrf # can calculate Earth's magnetic field for the magnetic torque function.
 
 ## Constants of the satellite
 m = 20.1 # mass (kg)
 cg = 0.01 * np.array([-0.51, 1.35, 2.02]) # center of gravity from geometric center (m)
+cp_initial = cg + np.array([0.03, -0.01, 0.02]) # initial center of pressure that will be randomized
+cp_mult = np.array([0.01, 0.01, 0.01]) # uncertainity in each axis
 inertia = np.diag([0.540, 0.518, 0.357]) # moment of inertia tensor (kg*m^2)
 r_altitude = 400e3 # altitude (m)
 r_earth = 6371e3 # radius of the Earth (m)
@@ -49,6 +52,13 @@ raan = np.deg2rad(45) # right ascension of ascending node (degrees to radians)
 inclination = np.deg2rad(51.6) # inclination of orbit (degrees to radians)
 dimensions = 0.01 * np.array([67.89, 22.63, 32.65]) # dimensions of the satellite (m)
 dipole_moment = np.array([0.2, 0.2, 0.2]) # magnetic dipole moment (A*m^2)
+AU = 1.496e11 # meters
+omega_earth = 2 * np.pi / (365.25 * 86400) # rad / s
+S_emit = 1361 # power emitted by sun (W / m^2)
+c = 3e8 # speed of light (m/s)
+P_srp = S_emit / c
+C_refl = 1.4 # average reflectivity for spacecraft (1.2 to 1.6)
+
 
 ## Control Law Constants
 dt_control = 0.1 # time step (s)
@@ -90,6 +100,15 @@ omega_max_y = (I_wheel/Iy) * smax_wheel
 omega_max_z = (I_wheel/Iz) * smax_wheel
 omega_max_vec = np.array([omega_max_x, omega_max_y, omega_max_z])
 
+def sample_cp():
+    delta = np.random.normal(0, cp_mult, size = 3)
+    return cp_initial + delta # creates a random center of pressure based on a gaussian normal distribution
+
+def random_quaternion():
+    q = np.random.normal(0, 1, 4) # creates a random quaternion based on gaussian normal distribution
+    return q / np.linalg.norm(q)
+
+r_cp = sample_cp()
 ## Quaternion math functions
 def quaternion_mult(q1, q2):
     w1, x1, y1, z1 = q1                                                 # quaternion is defined as w (real) and x,y,z (complex) values
@@ -184,19 +203,6 @@ def PID(q_current, omega_current, q_target, g_err, omega_max_vec):
     return torque_wheels, g_err_new
 ## External Torques
 
-torque_history = { 
-    "time": [],
-    "gravity_gradient": [],
-    "magnetic": [],
-    "drag": [],
-    "srp": [],
-    "wheels": [],
-    "pointing_error": [],
-    "omega": [],
-    "quaternion": [],
-    "total": []
-} # for data collection and plotting
-
 def gravity_gradient_torque(mu_earth, r_i, q, inertia): # r_i is position in inertial frame
     rnorm = np.linalg.norm(r_i)
     rhat_i = r_i / rnorm
@@ -210,13 +216,48 @@ def magnetic_torque(dipole, magfield_i, q): # magnetic field is inputted in iner
     T_magnetic = np.cross(dipole,magfield_b)
     return T_magnetic
 
-def drag_torque():
-    T_drag = np.array([0, 0, 0]) # not sure how to find drag yet, specifcally the position vector from COM to center of pressure
+def drag_torque(q, v_i, r_cp):
+    drag_rho = 3e-12
+    Cd = 2.2
+
+    v_b = inertial_to_body(v_i, q)
+    vmag = np.linalg.norm(v_b)
+
+    if vmag < 1e-10:
+        return np.zeros(3)
+    vhat = v_b / vmag
+
+    area = dimensions[0] * dimensions[1]
+
+    F_drag = -0.5 * area * drag_rho * Cd * vmag**2 * vhat
+    T_drag = np.cross(r_cp, F_drag)
     return T_drag
 
-def srp_torque():
-    T_srp = np.array([0, 0, 0]) # not sure how to find solar radiation pressure torque yet, same problem as drag torque
+def srp_torque(q, sun_i, r_cp):
+    sun_b = inertial_to_body(sun_i, q)
+    sun_mag = np.linalg.norm(sun_b)
+    
+    if sun_mag < 1e-10:
+        return np.zeros(3)
+    
+    sun_b_hat = sun_b / sun_mag
+    A_srp = dimensions[0] * dimensions[1]
+    F_srp = P_srp * C_refl * A_srp * sun_b_hat
+    T_srp = np.cross(r_cp, F_srp)
     return T_srp
+
+def shadow(r_i, sun_i): # check if the satellite is in earth's shadow
+    sun_norm = np.linalg.norm(sun_i)
+    if sun_norm < 1e-10:
+        return False
+    sun_hat = sun_i / sun_norm
+    proj = np.dot(r_i, sun_hat)
+    if proj > 0:
+        return False
+    # Check if Earth blocks the sun
+    d = np.linalg.norm(r_i - proj * sun_hat)
+    return d < r_earth
+
 
 
 ## dynamics simualation functions
@@ -267,18 +308,25 @@ def magnetic_field(r_i): # Simplfied magnetic field: https://en.wikipedia.org/wi
     B = (mu0 * M_earth/ (4 * np.pi * rnorm**3)) * ((3 * (mhat.dot(rhat))*rhat) - mhat)
     return B
 
-def state_vector_equation(t, y, torque_wheels, s_dot):
+def state_vector_equation(t, y, torque_wheels, s_dot, cp):
     q = y[0:4]
     omega = y[4:7]
     s = y[7:10]
     q = quaternion_norm(q)
+    r_cp = cp
 
     r_i, v_i = orbit_r_v_calculation(t)
 
     torque_mag = magnetic_torque(dipole_moment, magnetic_field(r_i), q)
     torque_grav = gravity_gradient_torque(mu_earth, r_i, q, inertia)
-    torque_drag = drag_torque() # need to finish
-    torque_srp = srp_torque() # need to finish
+
+    torque_drag = drag_torque(q, v_i, r_cp)
+    
+    sun_i = np.array([1.0, 0.0, 0.0])
+    if shadow(r_i, sun_i):
+        torque_srp = np.zeros(3)
+    else:
+        torque_srp = srp_torque(q, sun_i, r_cp)
 
     torque_total = torque_mag + torque_grav + torque_drag + torque_srp
     
@@ -290,9 +338,6 @@ def state_vector_equation(t, y, torque_wheels, s_dot):
     body_torques = torque_total + torque_wheels - np.cross(omega, (I_tot @ omega)) - np.cross(omega, E_wheel @ s)
     omega_dot = np.linalg.solve(I_tot, body_torques)
 
-    q_err = quaternion_error(q, q_target)
-    pointing_error = 2 * np.arccos(np.clip(q_err[0], -1, 1)) * (180/np.pi)
-
     dydt = np.zeros(10) # create the change in state vector
     dydt[0:4] = q_dot
     dydt[4:7] = omega_dot
@@ -300,12 +345,12 @@ def state_vector_equation(t, y, torque_wheels, s_dot):
 
     return dydt
 
-def rk4_integrator(func, t, y, dt, torque_wheels, s_dot):
+def rk4_integrator(func, t, y, dt, torque_wheels, s_dot, cp):
     y[0:4] = quaternion_norm(y[0:4])
-    k1 = func(t, y, torque_wheels, s_dot)
-    k2 = func(t + dt/2.0, y + dt/2.0 * k1, torque_wheels, s_dot)
-    k3 = func(t + dt/2.0, y + dt/2.0 * k2, torque_wheels, s_dot)
-    k4 = func(t + dt,     y + dt * k3, torque_wheels, s_dot)
+    k1 = func(t, y, torque_wheels, s_dot, cp)
+    k2 = func(t + dt/2.0, y + dt/2.0 * k1, torque_wheels, s_dot, cp)
+    k3 = func(t + dt/2.0, y + dt/2.0 * k2, torque_wheels, s_dot, cp)
+    k4 = func(t + dt,     y + dt * k3, torque_wheels, s_dot, cp)
 
     y_next = y + dt/6.0 * (k1 + 2*k2 + 2*k3 + k4)
     q_next = quaternion_norm(y_next[0:4])
@@ -314,71 +359,188 @@ def rk4_integrator(func, t, y, dt, torque_wheels, s_dot):
 
 ## Example Values
 
-q_initial = np.array([0.9, 0.1, 0.1, 0.1]) # random numbers for now
-q_initial = quaternion_norm(q_initial)
-omega_initial = np.array([0, 0, 0.01])
+q_initial = random_quaternion()
+omega_initial = np.random.uniform(-0.05, 0.05, 3)
 t_start = 0.0
-t_end = 600.0
-dt = 0.01
+t_end = 3600.0
+dt = 0.1
 s_initial = np.array([0.0, 0.0, 0.0])
 state_initial = np.concatenate([q_initial, omega_initial, s_initial])
 
-y = state_initial.copy()
 
-t = t_start
-last_control = -dt_control
-g_err_state = g_err.copy()
+N_mc = 10 # Monte-Carlo simulations
+max_error_mc = []
+final_error_mc = []
 
-torque_wheels = np.zeros(3)
-s_dot = np.zeros(3)
+torque_history = {
+    "time": [],
+    "gravity_gradient": [],
+    "magnetic": [], 
+    "drag": [],
+    "srp": [], 
+    "wheels": [], 
+    "pointing_error": [],
+    "omega": [],
+    "quaternion": [], 
+    "wheel_speeds": [],
+    "total": []
+}
 
-history = []
-times = []
-pos_history = []
-vel_history = []
+print(f"Starting {N_mc} Monte Carlo simulations")
+print(f"Simulation time: {t_end}s, timestep: {dt}s")
 
-while t < t_end:
-    
-    if t - last_control >= dt_control - 1e-9:
-        torque_wheels, g_err_state = PID(y[0:4], y[4:7], q_target, g_err_state, omega_max_vec)
-        s_dot = np.linalg.solve(E_wheel, torque_wheels)
-        last_control = t
+start_time_total = time.time()
 
-        # Save position and velocity
-        r_i, v_i = orbit_r_v_calculation(t)
-        torque_mag = magnetic_torque(dipole_moment, magnetic_field(r_i), y[0:4])
-        torque_grav = gravity_gradient_torque(mu_earth, r_i, y[0:4], inertia)
+for k in range(N_mc):
+    start_sim = time.time()
+    print(f"Simulation {k+1}/{N_mc}...", end='', flush=True)
 
+
+    cp_mc = sample_cp()
+    y = state_initial.copy()
+    t = t_start
+    last_control = -dt_control
+    g_err_state = g_err.copy()
+    torque_wheels = np.zeros(3)
+    s_dot = np.zeros(3)
+    max_error = 0.0
+
+    step = 0
+    save = int(dt_control / dt)
+
+    while t < t_end:
         q_err = quaternion_error(y[0:4], q_target)
-        pointing_error = 2 * np.arccos(np.clip(q_err[0], -1, 1)) * (180 / np.pi)
-        pos_history.append(r_i.copy())
-        vel_history.append(v_i.copy())
-        torque_history["time"].append(t)
-        torque_history["gravity_gradient"].append(torque_grav.copy())
-        torque_history["magnetic"].append(torque_mag.copy())
-        torque_history["wheels"].append(torque_wheels.copy())
-        torque_history["pointing_error"].append(pointing_error)
-        torque_history["omega"].append(y[4:7].copy())
-        torque_history["quaternion"].append(y[0:4].copy())
-    y = rk4_integrator(state_vector_equation, t, y, dt, torque_wheels, s_dot)
+        error = 2 * np.arccos(np.clip(q_err[0], -1, 1)) * 180 / np.pi
+        max_error = max(max_error, error)
+        
+        if t - last_control >= dt_control - 1e-9:
+            torque_wheels, g_err_state = PID(y[0:4], y[4:7], q_target, g_err_state, omega_max_vec)
+            s_dot = -np.linalg.solve(E_wheel, torque_wheels)
+            last_control = t
 
-    if np.any(np.isnan(y)) or np.any(np.abs(y[4:7]) > 10):
-        print(f"Unstable at t = {t}")
-        print(f"Omega: {y[4:7]}")
-        print(f"Torque: {torque_wheels}")
-        break
+            # Save position and velocity
+            if k == N_mc - 1:
+                r_i, v_i = orbit_r_v_calculation(t)
+                torque_mag = magnetic_torque(dipole_moment, magnetic_field(r_i), y[0:4])
+                torque_grav = gravity_gradient_torque(mu_earth, r_i, y[0:4], inertia)
+                torque_drag = drag_torque(y[0:4], v_i, cp_mc)
+                sun_i = np.array([1.0, 0.0, 0.0])
+                if shadow(r_i, sun_i):
+                    torque_srp = np.zeros(3)
+                else:
+                    torque_srp = srp_torque(y[0:4], sun_i, cp_mc)
 
-    t += dt
-    history.append(y.copy())
-    times.append(t)
 
-for key in ["gravity_gradient", "magnetic", "drag", "srp", "total", "wheels", "omega", "quaternion"]:
+                pointing_error = 2 * np.arccos(np.clip(q_err[0], -1, 1)) * (180 / np.pi)
+                torque_history["time"].append(t)
+                torque_history["gravity_gradient"].append(torque_grav.copy())
+                torque_history["magnetic"].append(torque_mag.copy())
+                torque_history["drag"].append(torque_drag.copy())
+                torque_history["srp"].append(torque_srp.copy())
+                torque_history["wheels"].append(torque_wheels.copy())
+                torque_history["pointing_error"].append(pointing_error)
+                torque_history["omega"].append(y[4:7].copy())
+                torque_history["quaternion"].append(y[0:4].copy())
+                torque_history["wheel_speeds"].append(y[7:10].copy())
+                torque_history["total"].append((torque_grav + torque_mag + torque_drag + torque_srp).copy())
+
+
+        y = rk4_integrator(state_vector_equation, t, y, dt, torque_wheels, s_dot, cp_mc)
+        
+
+        if np.any(np.isnan(y)) or np.any(np.abs(y[4:7]) > 10):
+            print(f"Unstable at t = {t}")
+            print(f"Omega: {y[4:7]}")
+            print(f"Torque: {torque_wheels}")
+            break
+
+        t += dt
+        step += 1
+
+    final_error_mc.append(error)
+    max_error_mc.append(max_error)
+    elapsed = time.time() - start_sim
+    print(f" Done in {elapsed:.1f}s | Final error: {error:.2f}°")
+
+
+time_total = time.time() - start_time_total
+for key in ["gravity_gradient", "magnetic", "drag", "srp", "total", "wheels", "omega", "wheel_speeds", "quaternion"]:
     torque_history[key] = np.array(torque_history[key])
 torque_history["time"] = np.array(torque_history["time"])
 torque_history["pointing_error"] = np.array(torque_history["pointing_error"])
 
 
 import matplotlib.pyplot as plt
+
+plt.show()
+
+print("Generating plots...")
+plt.figure(figsize=(10, 6))
+plt.hist(final_error_mc, bins=20, edgecolor='black')
+plt.xlabel("Worst-case pointing error (deg)", fontsize=12)
+plt.ylabel("Number of Monte-Carlo spacecraft", fontsize=12)
+plt.title(f"Monte-Carlo Simulation over varying center of pressure and initial quaternion (N={N_mc})", fontsize=14)
+plt.grid(True, alpha=0.3)
+plt.tight_layout()
+plt.show()
+
+
+# Using data from the very last Monte Carlo run (torque_history)
+time = torque_history["time"]
+errors = torque_history["pointing_error"]
+torques_env = torque_history["total"]
+torques_wheel = torque_history["wheels"]
+omega = torque_history["omega"]
+
+fig, axs = plt.subplots(2, 2, figsize=(14, 10))
+plt.subplots_adjust(hspace=0.3, wspace=0.25)
+
+# 1. Pointing Error (Is it converging?)
+axs[0, 0].plot(time, errors, color='red', lw=2)
+axs[0, 0].set_title("Pointing Error vs Time")
+axs[0, 0].set_ylabel("Degrees")
+axs[0, 0].grid(True, alpha=0.3)
+
+# 2. Wheel Torques (Are they saturated?)
+axs[0, 1].plot(time, torques_wheel[:, 0], label='X-Wheel')
+axs[0, 1].plot(time, torques_wheel[:, 1], label='Y-Wheel')
+axs[0, 1].plot(time, torques_wheel[:, 2], label='Z-Wheel')
+axs[0, 1].axhline(y=0.015, color='k', linestyle='--', alpha=0.5, label='Limit')
+axs[0, 1].axhline(y=-0.015, color='k', linestyle='--', alpha=0.5)
+axs[0, 1].set_title("Control Torque (Reaction Wheels)")
+axs[0, 1].set_ylabel("Nm")
+axs[0, 1].legend(loc='upper right', fontsize='small')
+axs[0, 1].grid(True, alpha=0.3)
+
+# 3. Environmental Torques (Are they too strong?)
+axs[1, 0].plot(time, torque_history["gravity_gradient"][:, 0], label='Gravity', alpha=0.7)
+axs[1, 0].plot(time, torque_history["drag"][:, 0], label='Drag', alpha=0.7)
+axs[1, 0].set_title("Disturbance Torques (X-Axis Example)")
+axs[1, 0].set_ylabel("Nm")
+axs[1, 0].legend(loc='upper right', fontsize='small')
+axs[1, 0].grid(True, alpha=0.3)
+
+# 4. Angular Velocity (Is the satellite tumbling?)
+axs[1, 1].plot(time, omega[:, 0], label='wx')
+axs[1, 1].plot(time, omega[:, 1], label='wy')
+axs[1, 1].plot(time, omega[:, 2], label='wz')
+axs[1, 1].set_title("Body Angular Velocity")
+axs[1, 1].set_ylabel("rad/s")
+axs[1, 1].legend(loc='upper right', fontsize='small')
+axs[1, 1].grid(True, alpha=0.3)
+
+axs[1, 0].clear()
+wheel_rpm = np.array(torque_history["wheel_speeds"]) * (60 / (2 * np.pi))
+axs[1, 0].plot(time, wheel_rpm[:, 0], label='Wheel X')
+axs[1, 0].plot(time, wheel_rpm[:, 1], label='Wheel Y')
+axs[1, 0].plot(time, wheel_rpm[:, 2], label='Wheel Z')
+axs[1, 0].axhline(y=3000, color='r', linestyle='--', label='Limit')
+axs[1, 0].set_title("Actual Reaction Wheel Speeds (RPM)")
+axs[1, 0].set_ylabel("RPM")
+
+plt.show()
+
+""""" Annotated for now, can change to see plots when needed.
 # Pointing Error over time
 plt.figure(figsize=(12,8))
 plt.subplot(3,1,1)
@@ -413,14 +575,16 @@ plt.grid(True)
 
 
 
-""""" Annotated for now, can change to see plots when needed.
+pos_history = np.array(pos_history)
+vel_history = np.array(vel_history)
+
 # plotting torques
 time = torque_history["time"]
 # Gravity Gradient Torque components
 plt.figure()
-plt.plot(time, torque_history["gravity_gradient"][:,0], label="Gravity X")
-plt.plot(time, torque_history["gravity_gradient"][:,1], label="Gravity Y")
-plt.plot(time, torque_history["gravity_gradient"][:,2], label="Gravity Z")
+plt.plot(torque_history["time"], torque_history["gravity_gradient"][:,0], label="Gravity X")
+plt.plot(torque_history["time"], torque_history["gravity_gradient"][:,1], label="Gravity Y")
+plt.plot(torque_history["time"], torque_history["gravity_gradient"][:,2], label="Gravity Z")
 plt.title("Gravity Gradient Torque Components")
 plt.xlabel("Time (s)")
 plt.ylabel("Torque (Nm)")
@@ -429,36 +593,15 @@ plt.grid(True)
 
 # Magnetic Torque components
 plt.figure()
-plt.plot(time, torque_history["magnetic"][:,0], label="Magnetic X")
-plt.plot(time, torque_history["magnetic"][:,1], label="Magnetic Y")
-plt.plot(time, torque_history["magnetic"][:,2], label="Magnetic Z")
+plt.plot(torque_history["time"], torque_history["magnetic"][:,0], label="Magnetic X")
+plt.plot(torque_history["time"], torque_history["magnetic"][:,1], label="Magnetic Y")
+plt.plot(torque_history["time"], torque_history["magnetic"][:,2], label="Magnetic Z")
 plt.title("Magnetic Torque Components")
 plt.xlabel("Time (s)")
 plt.ylabel("Torque (Nm)")
 plt.legend()
 plt.grid(True)
-
-# Position Components
-plt.figure()
-plt.plot(times, pos_history[:,0], label="Position X")
-plt.plot(times, pos_history[:,1], label="Position Y")
-plt.plot(times, pos_history[:,2], label="Position Z")
-plt.title("Position Components")
-plt.xlabel("Time (s)")
-plt.ylabel("Position (m)")
-plt.legend()
-plt.grid(True)
-
-# Velocity Components
-plt.figure()
-plt.plot(times, vel_history[:,0], label="Velocity X")
-plt.plot(times, vel_history[:,1], label="Velocity Y")
-plt.plot(times, vel_history[:,2], label="Velocity Z")
-plt.title("Velocity Components")
-plt.xlabel("Time (s)")
-plt.ylabel("Velocity (m/s)")
-plt.legend()
-plt.grid(True)
-"""
+"""""
+print("hi")
 plt.tight_layout()
 plt.show()
